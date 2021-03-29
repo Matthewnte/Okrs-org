c996cd37eb4172d2c520a7c8d1761eb8b3a8e020
Matthew Nte <Matthewnte1@gmail.com>
2021-03-29 17:28:54 +0100

    remove env not found crash

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,6 @@
-const dotenv = require('dotenv');
+require('dotenv').config();
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
 
 module.exports = {
   port: process.env.PORT,
