d5e23a04d3530004a47afb5a0f02722e3fb03a59
Matthew Nte <matthewnte1@gmail.com>
2021-04-29 04:34:40 +0100

    fix error handler

--- src/api/middleware/errorHandler.js
+++ src/api/middleware/errorHandler.js
@@ -50,9 +50,9 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     error.message = err.message;
 
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
