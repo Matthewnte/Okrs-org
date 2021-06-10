44b992c184ba03dcffad63fcb3f8cac2272b13fb
Matthew Nte <matthewnte1@gmail.com>
2021-06-10 16:52:21 +0100

    fix notification

--- src/services/notification.js
+++ src/services/notification.js
@@ -3,9 +3,8 @@ const FactoryService = require('./factory');
 
 const NotificationService = {
   getAllNotifications: async (query, userId) => {
-    const filter = { recepientUserId: { $in: ['6063e42c57c79e275848320b'] } };
+    const filter = { recepientUserId: { $in: [userId] } };
     const notifications = await FactoryService.getAll(Notification, query, filter);
-    console.log(notifications);
     return notifications;
   },
 };
